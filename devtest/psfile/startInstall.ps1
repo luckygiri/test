9980b78e0bd71766f72e607a1e6c3ee3cab75daa
luckygiri <gp2713@gmail.com>
2017-05-30 15:38:37 +0530

    Update startInstall.ps1

--- devtest/psfile/startInstall.ps1
+++ devtest/psfile/startInstall.ps1
@@ -1,5 +1,4 @@
 # Install
-
-Start-Process C:\SoftwaresDump\QTP12.5\setup.exe -ArgumentList '/quiet' -Wait
-Start-Process C:\SoftwaresDump\QTP12.5\qtpsetup.exe -ArgumentList '/quiet' -Wait
-
+Start-Process -FilePath "powershell.exe" -Verb runas
+Start-Process C:\SoftwaresDump\QTP12.5\setup.exe -ArgumentList '/s'
+Start-Process C:\SoftwaresDump\QTP12.5\qtpsetup.exe -ArgumentList '/s'
