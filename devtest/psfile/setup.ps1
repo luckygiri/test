bdcb4433c58a040e256187632111a9ca3140ca7c
luckygiri <gp2713@gmail.com>
2017-05-31 14:51:01 +0530

    Update setup.ps1

--- devtest/psfile/setup.ps1
+++ devtest/psfile/setup.ps1
@@ -1,10 +1,7 @@
-
-
 # Install
 
-Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe"
+Start-Process "C:\SoftwaresDump\QTP12.5\setup.exe" -ArgumentList " /v""/qn""" 
 
 Start-Sleep -Seconds "30"
 
-
-Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe"
+Start-Process "C:\SoftwaresDump\QTP12.5\qtpsetup.exe" -ArgumentList " /v""/qn""" 
