c8291be2e962c0e85f627e8d73a0d02d43b742c1
luckygiri <gp2713@gmail.com>
2017-06-07 17:40:37 +0530

    Update startinstall.ps1

--- devtest/VSCOM2015/startinstall.ps1
+++ devtest/VSCOM2015/startinstall.ps1
@@ -1,19 +1,77 @@
-$NewDIR = "C:\SoftwaresDump\QTP12.5"
-$SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
-$SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+[CmdletBinding()]
+param(
+)
 
-# Create temp directory
-New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
-# Download
-(New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
+###################################################################################################
 
-(New-Object System.Net.WebClient).DownloadFile("http://artifacts.g7crm4l.org/softwares/QTP12.5/vs2015.exe", "C:\SoftwaresDump\QTP12.5\vs2015.exe")
+#
+# PowerShell configurations
+#
 
+# NOTE: Because the $ErrorActionPreference is "Stop", this script will stop on first failure.
+#       This is necessary to ensure we capture errors inside the try-catch-finally block.
+$ErrorActionPreference = "Stop"
 
+# Ensure we set the working directory to that of the script.
+pushd $PSScriptRoot
 
-# Install
+###################################################################################################
 
-Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe"
-Start-Process "C:\SoftwaresDump\QTP12.5\vs2015.exe"  
+#
+# Functions used in this script.
+#
 
+function Handle-LastError
+{
+    $message = $error[0].Exception.Message
+    if ($message)
+    {
+        Write-Host -Object "ERROR: $message" -ForegroundColor Red
+    }
+    
+    # IMPORTANT NOTE: Throwing a terminating error (using $ErrorActionPreference = "Stop") still
+    # returns exit code zero from the PowerShell script when using -File. The workaround is to
+    # NOT use -File when calling this script and leverage the try-catch-finally block and return
+    # a non-zero exit code from the catch block.
+    exit -1
+}
 
+###################################################################################################
+
+#
+# Handle all errors in this script.
+#
+
+trap
+{
+    # NOTE: This trap will handle all errors. There should be no need to use a catch below in this
+    #       script, unless you want to ignore a specific error.
+    Handle-LastError
+}
+
+###################################################################################################
+
+#
+# Main execution block.
+#
+
+try
+{
+    $NewDIR = "C:\SoftwaresDump\QTP12.5"
+    $SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
+    $SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+
+    # Create temp directory
+    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -ErrorAction SilentlyContinue
+    # Download
+    (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
+    (New-Object System.Net.WebClient).DownloadFile("http://artifacts.g7crm4l.org/softwares/QTP12.5/vs2015.exe", "C:\SoftwaresDump\QTP12.5\vs2015.exe")
+
+    # Install
+    Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+    Start-Process "C:\SoftwaresDump\QTP12.5\vs2015.exe"  
+}
+finally
+{
+    popd
+}
