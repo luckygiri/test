f52f35906ad54b73854e2e9f18e0650d40d03e07
luckygiri <gp2713@gmail.com>
2017-06-07 18:23:10 +0530

    Update startinstall.ps1

--- devtest/VSCOM2015/startinstall.ps1
+++ devtest/VSCOM2015/startinstall.ps1
@@ -61,17 +61,22 @@ try
     $SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
     $SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
 
-    # Create temp directory
-    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -Force
-    # Download
+    Write-Output 'Preparing temp directory ...'
+    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -Force | Out-Null
+
+    Write-Output 'Downloading pre-requisite files ...'
     (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
     (New-Object System.Net.WebClient).DownloadFile("http://artifacts.g7crm4l.org/softwares/QTP12.5/vs2015.exe", "C:\SoftwaresDump\QTP12.5\vs2015.exe")
 
-    # Install
-    Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe" 
+    Write-Output 'Installing ...'
+    Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe"
     Start-Process "C:\SoftwaresDump\QTP12.5\vs2015.exe"  
+
+    Write-Output 'Done!'
 }
 finally
 {
     popd
 }
+
+
