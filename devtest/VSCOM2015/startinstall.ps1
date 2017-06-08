420b5178c8029839f4973313d838e258d369d52a
luckygiri <gp2713@gmail.com>
2017-06-08 11:05:58 +0530

    Create startinstall.ps1

--- devtest/VSCOM2015/startinstall.ps1
+++ devtest/VSCOM2015/startinstall.ps1
@@ -57,19 +57,19 @@ trap
 
 try
 {
-    $NewDIR = "C:\SoftwaresDump\QTP12.5"
+    $NewDIR = "C:\SoftwaresDump"
     $SoftwareWebLink = "https://download.microsoft.com/download/e/4/c/e4c393a9-8fff-441b-ad3a-3f4040317a1f/vs_community.exe"
-    $SoftwarePath = "C:\SoftwaresDump\QTP12.5\vs_community.exe"
+    $SoftwarePath = "C:\SoftwaresDump\vs_community.exe"
 
     Write-Output 'Preparing temp directory ...'
-    New-Item "C:\SoftwaresDump\QTP12.5" -ItemType Directory -Force | Out-Null
+    New-Item "C:\SoftwaresDump" -ItemType Directory -Force | Out-Null
 
     Write-Output 'Downloading pre-requisite files ...'
     (New-Object System.Net.WebClient).DownloadFile("$SoftwareWebLink", "$SoftwarePath")
    
 
  Write-Output 'Installing ...'
-Start-Process "C:\SoftwaresDump\QTP12.5\vs_community.exe" -ArgumentList '/q' -Wait 
+Start-Process "C:\SoftwaresDump\vs_community.exe" -ArgumentList '/q' -Wait 
 
     
 
